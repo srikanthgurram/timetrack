df34ff8865cc6e25069b3a34d77d08ca3c4bd0de
Srikanth Gurram <2gurram@gmail.com>
2014-07-31 15:48:45 +0530

    added default index files and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +14,11 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :companies
+  resources :projects
+  resources :clients
+  resources :employees
+  resources :works
 
   # Example resource route with options:
   #   resources :products do
