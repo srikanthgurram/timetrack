d3f2ba3748dc844244157a1086317b1f95d7fdc7
Srikanth Gurram <2gurram@gmail.com>
2014-08-07 15:44:52 +0530

    updated employees info and navigation links

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,8 +6,10 @@ class EmployeesController < ApplicationController
 	def show
 		if (params[:id])
 			@employee = Employee.exists?(params[:id]) ? Employee.find(params[:id]) : nil
+			@employee_works = @employee.works.paginate(:page => params[:page], :per_page => 10)
 		elsif(params[:slug])
 			@employee = (Employee.find_by username: params[:slug])? (Employee.find_by username: params[:slug]): nil
+			@employee_works = @employee.works.paginate(:page => params[:page], :per_page => 10)
 		end
 	end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
 	def index
-		@works = Work.all.paginate(:page => params[:page], :per_page => 10)
+		@works = Work.all.paginate(:page => params[:page], :per_page => 10).order(date_time_performed: :desc)
 	end
 
 	def show
